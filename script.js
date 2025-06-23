879201c4df0257d316ac6a19a6007818e03f091f
Amel252 <amelghanem25@gmail.com>
2025-06-23 22:09:21 +0200

    Travailler l'image cover son opacity en scoll

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+
+// Lorsque tu cliques sur l’icône .fa-bars, le script ajoute ou enlève la classe .show-menu à l'élément menuEl.
+
 const barEl = document.querySelector(".fa-bars");
 const menuEl = document.querySelector(".nav-menu");
 
 barEl.addEventListener("click", () => {
     menuEl.classList.toggle("show-menu");
 });
-// Lorsque tu cliques sur l’icône .fa-bars, le script ajoute ou enlève la classe .show-menu à l'élément menuEl.
+
+
+// Function pour travailler container de cover
+const bgImageEl = document.getElementById("bg-image");
+
+window.addEventListener("scroll", () =>{
+    updateImage()
+})
+// dans cette function on veux diminuer l'opacity de notre Img
+function updateImage() {
+    bgImageEl.style.opacity = 1 - window.pageYOffset / 800
+}
